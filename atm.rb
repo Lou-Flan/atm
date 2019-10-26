15fd7fa0babeb748dbd0e79e8f081494aa26e318
Louise <louise_flanagan@outlook.com>
2019-10-26 15:51:29 +1100

    initial commit

---
+++ atm.rb
@@ -0,0 +1,20 @@
+# Create an application that functions like an ATM.
+# 1. Write out a list of features
+# 2. In the list, mark each feature as 'essential' or 'nice to have'
+# 3. Decide what the simplest possible version of your ATM would look like
+# 4. Show Bianca or Alex and get their thoughts on whether it should be simpler
+# 5. Create a new directory on your computer, initialise it as a git repo, 
+# and start coding that simplest version! Remember to add and commit your code regularly, 
+# including every time you get something working
+
+#Essential
+# Balance
+# Withdraw
+
+#Bonus
+# User id
+# pin
+
+balance = 9999
+pin = 1234
+
