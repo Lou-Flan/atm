216e0c6614ba76810f4d37f64d107493ba7a43a2
Louise <louise_flanagan@outlook.com>
2019-10-28 22:20:16 +1100

    user defined pin working

--- atm.rb
+++ atm.rb
@@ -7,74 +7,60 @@
 # and start coding that simplest version! Remember to add and commit your code regularly, 
 # including every time you get something working
 
-puts "New card account detected. Please setup your pin to access your account."
+############### USER PIN CREATION ###############
 
-pin = []
-while pin == []
-puts "Please enter a four digit pin"
-new_user = gets.chomp.to_i
+puts "New card account detected."
 
-puts "Please re-enter your pin to confirm"
-confirmation = gets.chomp.to_i
+pin = 0
+while pin == 0
+    puts "Please create a four digit pin"
+    new_user = gets.chomp.to_i
 
-    if confirmation == new_user
-        pin = [new_user]
+    if new_user.between? 1000, 9999
+        pin = new_user
     else 
-        puts "Those didn't match, please try again."
+        puts "Invalid. Please try again."
     end
 end
 
+#################### ATM ####################
 
 starting_balance = rand 99999
-pin = 1234
 withdraw_options = [10, 20, 50, 100]
 
-
-puts "Card detected. Please enter your pin"
-user_pin = 0
+puts "Pin setup complete. Please enter your new pin to access your account"
 
 pin_attempts = 0
-while pin_attempts < 3 && user_pin != pin
-    puts "Please enter your pin"
-    user_pin = gets.chomp.to_i
-
-
-        pin_attempts += 1  
-        if pin_attempts < 3 && user_pin != pin
-            puts "Incorrect pin, please try again. #{3 - pin_attempts} attempts remaining."
-
-            user_pin
-        elsif user_pin != pin && pin_attempts == 3
-            puts "Please contact your bank."
-          exit
-        end
+while pin_attempts <= 2
+auth = gets.chomp.to_i
+pin_attempts += 1
     
-    end
-
-    if user_pin == pin
+if auth == pin
     puts "Correct pin! Your balance is $#{starting_balance}"
-   
-       attempts = 0 
-       while attempts <= 1         
-        puts "How much would you like to withdraw? #{withdraw_options}"
-        withdraw_amount = gets.chomp.to_i
-        attempts += 1 
-
+        
+    actions = 0 
+    while actions <= 2         
+        puts "How much would you like to withdraw? Notes available are #{withdraw_options}"
+        withdraw_amount = gets.chomp.to_i 
+        actions += 1
+            
         if withdraw_amount > starting_balance
-            puts "Insufficient funds, your balance is #{starting_balance} please make another selection"
-
+            puts "Insufficient funds, your balance is #{starting_balance}."
         elsif (withdraw_amount%10) == 0
-            puts "Withdrawing #{withdraw_amount}"
+            puts "Withdrawing $#{withdraw_amount}"
             puts "Your balance is now $#{starting_balance -= withdraw_amount}."
             puts "Have a nice day!"
-            break 
-        elsif (withdraw_amount%10) != 0
-                puts "ERROR! please select a multiple of 10"
+            exit 
         else
-                puts "Please make a selection"    
-            end
-        
+            puts "ERROR! please select a multiple of $10"    
+        end
+    end
+    
+elsif auth != pin
+    puts "Incorrect. Please re-enter your pin. #{3 - pin_attempts} attempts remaining."
 
-    end  
-end
+else 
+    exit
+    end    
+ end
 
